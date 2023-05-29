63178712e508563a161b494f6677dc2f7ee17834
BloodHoundPP <prasadrpatil23@gmail.com>
2023-05-29 11:34:55 +0530

    game created

---
+++ DiceGame.java
@@ -0,0 +1,6 @@
+import java.rmi.*;
+
+public interface DiceGame extends Remote { 
+	public int rollDice() throws RemoteException;
+	public int compareRolls(int roll1, int roll2) throws RemoteException;
+}
